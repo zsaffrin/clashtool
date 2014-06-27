81f9998ec09c250ea770d181b65b64b7157dc129
Zach Saffrin <zach@zachsaffrin.com>
2014-06-27 10:37:35 -0400

    Removed debug text, added Dashboard placeholder text

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,19 +1,13 @@
 <div class="section">
 	<div class="container">
-		<div class="size-1-3 grid-drop">
-			<p>This is the Home/index View!
-			<?php
-				Home::showMessage();
-
-				if (isset($user)) {
-					echo '<p>User Status: <b>'.$user->level.'</b><br>';
-					print_r($user);
-				} else {
-					echo '<p>No user object found';
-				}
-			?>
+		<div class="size-1-1">
+			<?php Home::showMessage(); ?>
+			<div class="size-1-1">
+				<p>This is the Home/index View 
+				<?php if ($user->level>1) { ?>
+					<p>When a user is logged in, this view will be replaced by the Dashboard once the My Base functionality is built 
+				<?php } ?>
+			</div>
 		</div>
 	</div>
 </div>
-
-
