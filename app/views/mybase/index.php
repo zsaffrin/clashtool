4e53a20c87aedbc15d43c40de621f9b7fceaac00
Zach Saffrin <zach@zachsaffrin.com>
2014-08-22 15:51:37 -0400

    Improved My Base dashboard tiles and layout

--- app/views/mybase/index.php
+++ app/views/mybase/index.php
@@ -4,30 +4,59 @@
 		<?php $this->showMessage(); ?>
 
 		<div class="size-1-1">
-			<h2>My Base</h2>
+			<h3 class="no-space">My Base</h3>
+			<h2 class="mybase-sect-title">Dashboard</h2>
 		</div>
 
 		<div class="size-1-1">
-			<h4>Production</h4>
-			<table>
-				<?php 
-					$hours = array(1, 2, 4, 8, 12, 16);
-					
-					echo '<tr>';
-					echo '<th>&nbsp;';
-					foreach ($hours as $h) {
-						echo '<td>'.$h;
-					}
-
-					foreach ($this->production as $res) {
-						echo '<tr>';
-						echo '<th>'.$res['name'].' ';
-						foreach ($hours as $h) {
-							echo '<td>'.abs($res['count'] * $h).' ';
-						}
-					}
-				?>
-			</table>
+			<div class="size-1-1">
+				<h3 class="mybase-sect-title half-space">Production</h3>
+			</div>
+			<div class="size-1-3">
+				<div class="mybase-tile gold">
+					<span class="title">Gold</span>
+					<table>
+						<?php
+							$hours = array(1, 4, 8, 12, 16);
+							foreach ($hours as $h) {
+								echo '<tr>';
+								echo '<th>'.$h.'h';
+								echo '<td>'.number_format(abs($this->production[1]['count'] * $h)).' ';
+							}
+						?>
+					</table>
+				</div>
+			</div>
+			<div class="size-1-3">
+				<div class="mybase-tile purple">
+					<span class="title">Elixir</span>
+					<table>
+						<?php
+							$hours = array(1, 4, 8, 12, 16);
+							foreach ($hours as $h) {
+								echo '<tr>';
+								echo '<th>'.$h.'h';
+								echo '<td>'.number_format(abs($this->production[2]['count'] * $h)).' ';
+							}
+						?>
+					</table>
+				</div>
+			</div>
+			<div class="size-1-3">
+				<div class="mybase-tile black">
+					<span class="title">Dark Elixir</span>
+					<table>
+						<?php
+							$hours = array(1, 4, 8, 12, 16);
+							foreach ($hours as $h) {
+								echo '<tr>';
+								echo '<th>'.$h.'h';
+								echo '<td>'.number_format(abs($this->production[3]['count'] * $h)).' ';
+							}
+						?>
+					</table>
+				</div>
+			</div>
 		</div>
 
 	</div>
